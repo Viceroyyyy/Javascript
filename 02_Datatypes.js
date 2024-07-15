0ffb2fb432e2472cbcd14f9c4788651119b40e6d
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-07-15 21:27:00 +0530

    Datatypes intro

---
+++ 02_Datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //Treats all the code as the newer version of JS
+
+// alert(3+3) // This is used when we are using Javascript using Browser
+//Gives a pop in the form of Dialog Box
+
+//Datatypes
+/*
+number
+bigint
+string => " "
+boolean => true/false
+null => standalone value
+undefined
+symbol => unique values,used  while using react
+object
+*/
+let age=15
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof age)
+
