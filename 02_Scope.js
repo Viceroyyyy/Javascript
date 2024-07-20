b019c248a9acb8eed54d342ea1da03a78affc2c4
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-07-20 10:42:51 +0530

    Local,Global Scope and Scope in nested functions

--- 02_Scope.js
+++ 02_Scope.js
@@ -22,3 +22,54 @@ if(true){
 console.log(c); //Gives output 30
 console.log(b); //Throws error 
 console.log(a); //Throws Error
+
+/////////////////////////////////////////////////////////////////////////
+//------------------------------NESTED SCOPE------------------------------------------------
+
+function nestedscope() {
+    const username = "Devesh";
+
+    function nestedscope2() {
+        const loggedIn="true";
+        console.log(username);
+    }
+    //console.log(loggedIn); // Gives Error because loggenIn variable is locally declared under second function
+
+    nestedscope2();
+}
+nestedscope();
+
+//Closure means that in nested functions, child functions nd can use the variables of the Parent functions 
+//There's more to Closure
+
+///Two Ways of declaring a function
+
+//Method 1 Traditional way
+addOne(6); //This works fineee no error unlike variable based function declaration
+function addOne(num){
+    console.log(num+1);
+}
+
+addOne(5);
+
+
+//Method 2 Declaring function in a Variable
+
+variable(8);//Throws errpr because variable is cannot be used before initialization
+//ReferenceError: Cannot access 'variable' before initialization
+
+
+let variable = function add(num){
+    console.log(num+1);
+}
+
+//For method two  we pass the value in variable ie:-
+ variable(8);
+ add(9); //Throws error because we cannot acces the function declared in a variable directly
+
+ //console.log(variable = 8);
+
+
+
+
+
