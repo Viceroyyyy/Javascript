bf64b371bd29245e8e5b6b2cb993031ed930f876
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-07-25 00:02:42 +0530

    Strings declaration an methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,10 @@
+//---------Strings--------------------------------
+
+const name="Devesh";
+const gender = "Male";
+console.log("Using normal Method:-");
+console.log("Hi! My name is "+name+" and i am a "+gender); //Old/Ancient method to print Strings
+
+//New Method involves using String Interpolation i.e using Backticks(`)
+console.log(`Using String Interpolation`);
+console.log(`Hi! My name is ${name} and i am a ${gender}`);
