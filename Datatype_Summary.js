41041fd8e8e7aef8b31af2cee52438efb40ed782
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-07-24 07:39:49 +0530

    Datatype_summaary and Memory allocation

--- Datatype_Summary.js
+++ Datatype_Summary.js
@@ -29,3 +29,30 @@ const myfunc = function(){
     console.log("Hello world!");
 };
 console.log(myfunc());
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//---------------------------How is memory allocated?---------------------------------
+
+//Stack is used for Primitive Datatypes and a copy of variable is provided to use.
+//Heap memory is allOocated for Non-Primitive Datatypes and a reference of original value is provided i.e original value will be changed in case of any changes
+
+//Example:
+ let name="Devesh";
+ let anothername = name; //keeps a copr of name
+ anothername = "Shweepy" ; //copy  is changed whereas original remains same
+console.log(name);
+console.log(anothername);
+
+//Using Non primitive datatypes for heap memory
+ let obj1={
+    email: "foo@example.com",
+ }
+
+ let obj2=obj1;
+ console.log(obj1.email);
+ console.log(obj2.email);
+  obj2.email = "deepak@example.com";
+
+  console.log(obj1.email);
+ console.log(obj2.email);
