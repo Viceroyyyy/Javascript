68857c2642fe3d775f73719abd764caa02c68fe2
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-07-28 22:29:09 +0530

    While and Do while loops

---
+++ 05_Iterations/two.js
@@ -0,0 +1,19 @@
+//While loop
+
+let arr = ["Devesh","Shweepy","Love"];
+ let index=0;
+
+ while(index<arr.length)
+ {
+    console.log(`The element at index ${index} is ${arr[index]}`);
+    index+=1;
+ }
+
+ do
+ {
+    console.log(`The index is  ${index}`);
+    index++;
+
+ }while(index<=20);
+
+ 
