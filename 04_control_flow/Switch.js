337dbf623397eb09987011484c76e5bc849da72d
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-07-28 19:51:50 +0530

    Switch case

---
+++ 04_control_flow/Switch.js
@@ -0,0 +1,24 @@
+//SWITCH CASE
+//Copy and  USe Shortcut key shift+option+down arrow key
+let month="Jan";
+switch(month){
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "Mar":
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        // break;
+    default:
+        console.log("Not found")    
+}
+
+//if there's no break then everything below that will be printed
