d491b8c55d739f2c7a35a5c6db9417b40bf8f0fa
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-07-28 19:43:42 +0530

    Basic Iif -else in control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,28 @@
+//if
+//<,>,<=,>=,==,!=,===,!==
+/*
+if(condition){
+
+}
+else{
+
+}
+*/
+
+// A bad way to write code.....
+if(true) console.log("Devesh"),console.log("Shweepy");
+
+if(true) console.log("Devesh"),
+console.log("Shweepy");
+
+/*
+if(condition){
+Statement;
+}
+else if(condition){
+Statement;
+}
+else{
+Statement;
+}
+*/
