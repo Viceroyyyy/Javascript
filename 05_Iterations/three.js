41fb3dc253ff3165dd78fbc542dec869226dbb32
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-07-30 22:54:57 +0530

    For Of loops and Maps

---
+++ 05_Iterations/three.js
@@ -0,0 +1,46 @@
+// For of Loop
+//Used to iterate easily over arrays objects etc
+
+let arr= [1,2,3,4,5,6,7];
+for (const index of arr) {
+    console.log(`the value is ${index}`);
+}
+
+//above loop runs exactly same as ForEach Loop
+
+const str = "Devesh Agnihotri";
+for (const st of str) {
+    console.log(`the character is ${st}`);
+
+}
+
+//Maps => It is an object
+// it consists of key and value pairs
+
+const mp = new Map(); //Consists of unique values
+
+mp.set("USA","United States Of America");
+mp.set("IN", "India");
+mp.set("JP","Japan");
+
+console.log(mp);
+
+// For of loop on maps
+for(const [key, value] of mp) {
+    console.log(key,":-",value);
+}
+
+// For of doesn't iterate objects
+//For eg:-
+let myobj = {
+    "game1" : "IGI",
+    "game2" : "NFS"
+}
+//Not the code written below will give error
+for(const [key, value] of myobj) {
+    console.log(key,":",value);
+}
+
+// TypeError: myobj is not iterable
+
+ 
