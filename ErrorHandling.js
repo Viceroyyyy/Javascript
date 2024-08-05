feb41788dbb617d79dbe48ecaf8a3c3c41ce2da0
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-08-05 19:45:00 +0530

    Error Handling

--- ErrorHandling.js
+++ ErrorHandling.js
@@ -39,3 +39,21 @@ There are some other errors in JavaScript. These other errors include:
 
 
 */
+
+function  addTwoNums(a,b){
+    try{
+        if(typeof(a)!="number")
+        throw new ReferenceError("the first argument is not a number");
+        else if(typeof(b)!="number")
+        throw new ReferenceError("the second argument is not a number");
+        else
+        console.log(a+b);
+    }
+    catch(err){
+        console.log("Error!",err);
+    }
+
+}
+console.log("It still works")
+
+addTwoNums(5,"5");
