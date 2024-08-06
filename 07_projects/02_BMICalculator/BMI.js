efbbf14180b38ee93a2f32b17b43a9520d8cebed
Viceroyyyy <120083048+Viceroyyyy@users.noreply.github.com>
2024-08-06 11:32:07 +0530

    BMI javascript

--- 07_projects/02_BMICalculator/BMI.js
+++ 07_projects/02_BMICalculator/BMI.js
@@ -25,9 +25,9 @@ form.addEventListener('submit', function(e){
        if(BMI<18.6)
         finalresult.innerHTML = `You are Underweight according to your BMI`;
         else if (BMI>=18.6 && BMI<=24.9)
-            finalresult.innerHTML = `You are Healthy according to you BMI`;
+            finalresult.innerHTML = `You are Healthy according to your BMI`;
         else 
-        finalresult.innerHTML = `You are Overweight. Please pay attention to you health`;
+        finalresult.innerHTML = `You are Overweight. Please pay attention to your health`;
     }
 
     //To print the final result based on Weight Guide
